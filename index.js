4eab866101798caceb0ab61c884456691138f2d0
mazkarisk <mazkarisk@gmail.com>
2022-07-06 22:45:44 +0900

    test

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/src/index.html')))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
