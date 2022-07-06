6bbcdff554225e05e55611c08675be7747b899c1
mazkarisk <mazkarisk@gmail.com>
2022-07-07 00:50:57 +0900

    test

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+// インポート
 const express = require('express');
 const http = require('http');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+const socketIo = require('socket.io');
 
+// 初期化
+const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.Server(app);
+const io = socketIo(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/src/index.html');
@@ -12,4 +15,18 @@ app.get('/', (req, res) => {
 
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
+});
+
+// クライアントとのコネクションが確立したら'connected'という表示させる
+io.on('connection', (socket) => {
+  console.log('user connected');
+
+  // 'sendMessage' というイベント名で受信できる
+  // 第一引数には受信したメッセージが入り、ログに出力する
+  socket.on('sendMessage', (message) => {
+    console.log('Message has been sent: ', message);
+    
+    // 'receiveMessage' というイベントを発火、受信したメッセージを全てのクライアントに対して送信する
+    io.emit('receiveMessage', message);
+  });
 });
