eed3142f6d56fc4e473b0ee4d535f40bb241485b
mazkarisk <mazkarisk@gmail.com>
2022-07-09 13:56:40 +0900

    test

--- index.js
+++ index.js
@@ -9,24 +9,33 @@ const app = express();
 const server = http.Server(app);
 const io = socketIo(server);
 
+// webサーバーの設定
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/src/index.html');
+	res.sendFile(__dirname + '/src/index.html');
 });
-
+app.get('/test', (req, res) => {
+	res.sendFile(__dirname + '/src/test.html');
+});
 server.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+	console.log(`listening on port ${PORT}`);
 });
 
-// クライアントとのコネクションが確立したら'connected'という表示させる
+// ソケット通信の設定
 io.on('connection', (socket) => {
-  console.log('user connected');
+	console.log('user connected');
 
-  // 'sendMessage' というイベント名で受信できる
-  // 第一引数には受信したメッセージが入り、ログに出力する
-  socket.on('sendMessage', (message) => {
-    console.log('Message has been sent: ', message);
-    
-    // 'receiveMessage' というイベントを発火、受信したメッセージを全てのクライアントに対して送信する
-    io.emit('receiveMessage', message);
-  });
+	// 初回接続時にクライアントからくる、接続IDの要求に対する処理
+	socket.on('requestId', (message) => {
+		io.emit('responseId', socket.id);
+		console.log('新規接続開始。ソケットID： ', socket.id);
+	});
+
+	// 'sendMessage' というイベント名で受信できる
+	// 第一引数には受信したメッセージが入り、ログに出力する
+	socket.on('sendMessage', (message) => {
+		console.log('Message has been sent: ', message);
+		
+		// 'receiveMessage' というイベントを発火、受信したメッセージを全てのクライアントに対して送信する
+		io.emit('receiveMessage', message);
+	});
 });
